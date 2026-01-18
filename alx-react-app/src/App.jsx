67bc4a62fe1ffb207980f558fe6b86fbddd6b58d
Kidus Dawit <KidusDawit1921@gmail.com>
2026-01-18 10:08:28 +0300

    Initial React setup

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,20 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  return (
-    <div>
-      <WelcomeMessage />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
