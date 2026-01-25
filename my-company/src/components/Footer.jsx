d22401521a7cafe6b1034dce44004640fcc4e9c8
Kidus Dawit <kidusdawit1921@gmail.com>
2026-01-25 23:28:01 +0300

    Add files via upload

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: '#222',
+        color: 'white',
+        textAlign: 'center',
+        padding: '10px',
+        marginTop: '40px'
+      }}
+    >
+      <p>© 2026 My Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
