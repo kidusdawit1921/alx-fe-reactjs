34116f1d797622e06d6a4179556c4306ee67d651
Kidus Dawit <kidusdawit1921@gmail.com>
2026-02-06 17:26:40 +0300

    Add files via upload

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
